b860cc8126697c0de92a82b738210490c3311dee
PetrMFIT <xmolikp00@stud.fit.vutbr.cz>
2025-10-01 12:02:54 +0200

    token definition in header

---
+++ lex_scanner.h
@@ -0,0 +1,68 @@
+#ifndef LEX_SCANNER_H
+#define LEX_SCANNER_H
+
+typedef enum State{
+    Start,
+}State;
+
+typedef enum TokenType{
+    KEYWORD_class,
+    KEYWORD_if,
+    KEYWORD_else,
+    KEYWORD_is,
+    KEYWORD_null,
+    KEYWORD_return,
+    KEYWORD_var,
+    KEYWORD_while,
+    KEYWORD_Ifj,
+    KEYWORD_static,
+    KEYWORD_import,
+    KEYWORD_for,
+    KEYWORD_Num,
+    KEYWORD_String,
+    KEYWORD_Null,
+    IDENTIFIER_LOCAL,
+    IDENTIFIER_GLOBAL,
+    INT,
+    FLOAT,
+    STRING,
+    PROLOG,
+    TIMES,
+    DIVIDE,
+    PLUS,
+    MINUS,
+    LESSER,
+    GREATER,
+    LESSER_EQUAL,
+    GREATER_EQUAL,
+    EQUAL,
+    NOT_EQUAL,
+    ASSIGN,
+    L_ROUND,
+    R_ROUND,
+    L_CURLY,
+    R_CURLY,
+    IFJ_READ_STR,
+    IFJ_READ_NUM,
+    IFJ_WRITE,
+    IFJ_FLOOR,
+    IFJ_STR,
+    IFJ_LENGTH,
+    IFJ_SUBSTRING,
+    IFJ_STRCMP,
+    IFJ_ORD,
+    IFJ_CHR,
+    T_EOF
+}TokenType;
+
+typedef struct Token{
+    TokenType type;
+    char *lexeme;
+    union value{
+        int int_val;
+        double float_val;
+        char *string_val;
+    };
+}Token;
+
+#endif
